ca211d3944a8007458656f57e0b80de3358b3c56
Alex Lee <hyunho98@gmail.com>
2023-07-13 16:46:50 -0400

    commit tree ef18d92640d7eff075d07f123dd96f29b375f124

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+    ("Mr. Chocolate", 10, "M", "Red", "Angry", 1),
+    ("Rowdy", 20, "F", "Orange", "Curious", 1),
+    ("Tabitha", 30, "M", "Yellow", "Hungry", 1),
+    ("Sergeant Brown", 40, "F", "Green", "Jealous", 1),
+    ("Melissa", 50, "M", "Blue", "Sad", 0),
+    ("Grinch", 60, "F", "Indigo", "Feisty", 0),
+    ("Wendy", 70, "M", "Violet", "Pensive", 0),
+    (null, 80, "F", "Polar", "Cold", 0);
